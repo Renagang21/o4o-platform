22a0a49da9606800a3d32a1c2126870533cef023
Renagang21 <sohae2100@gmail.com>
2025-11-08 18:31:38 +0900

    fix(dropshipping): Fix typo in RoleApplicationForms (manager Name -> managerName)

--- apps/main-site/src/components/dropshipping/RoleApplicationForms.tsx
+++ apps/main-site/src/components/dropshipping/RoleApplicationForms.tsx
@@ -23,7 +23,7 @@ export const SupplierApplicationForm: React.FC = () => {
     businessAddress: '',
     businessPhone: '',
     taxEmail: '',
-    manager Name: '',
+    managerName: '',
     managerPhone: '',
     managerEmail: '',
     // Supplier specific
